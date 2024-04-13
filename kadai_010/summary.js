17123f37517c465b3dfb67709d4ea9b8a754002b
shu <magic5052@outlook.com>
2024-04-13 22:00:52 +0900

    kadai_010 finished

---
+++ kadai_010/summary.js
@@ -0,0 +1,23 @@
+$(function () {
+
+    // 色が変わる
+    $('#change-color').on('click', function () {
+        $('#target').css('color', 'red');
+    });
+
+    // 文字が切り替わる
+    $('#change-text').on('click', function () {
+        $('#target').text('Hello!');
+    });
+
+    // フェードアウトで文字が消える
+    $('#fade-out').on('click', function () {
+        $('#target').fadeOut();
+    });
+
+    // フェードインで文字が現れる
+    $('#fade-in').on('click', function () {
+        $('#target').fadeIn();
+    });
+
+})
